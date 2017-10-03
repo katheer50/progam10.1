a1879f3aadf934b2e7dd79968ad97149a102974d
unknown <leop3f24@gmail.com>
2017-10-03 16:47:23 +0800

    program10.1

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.concurrent.SynchronousQueue;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	String data0,data1,data2,data3,data4,data5;
+    data1="陳亦禹";
+    data2="106024050";
+    data3="資訊傳播學系";
+	//System.out.print("姓名:"+data1+"\n學號:"+data2+"\n系別:"+data3);
+    //System.out.print("請輸入你的性別:\n");
+    //System.out.print("請輸入你的生日:\n");
+    //System.out.println("請輸入你的星座");
+    //Scanner sc=new Scanner(System.in);
+    //data0=sc.next();
+    //data4=sc.next();
+    //data5=sc.next();
+    //System.out.println("性別:"+data0);
+    //System.out.println("生日:"+data4);
+    //System.out.println("星座:"+data5);
+        System.out.printf("你的姓名是:陳亦禹");
+
+
+    }
+}
